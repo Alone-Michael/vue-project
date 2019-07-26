70e3de51d95a0cb54c99e6fe7a3b00531164774d
”Alone <2796547009@qq.com>
2019-07-26 16:16:53 +0800

    vue-项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style>
+#app {
+  padding-top: 40px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="home">
+    <!-- 顶部Header区域 -->
+    <header class="mui-bar mui-bar-nav">
+      <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+      <h1 class="mui-title">一个神奇的网站</h1>
+    </header>
+
+    <!-- 中间的路由区域 -->
+
+    <!-- 底部tabBar区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email">
+          <span class="mui-badge">9</span>
+        </span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+       
+    };
+  },
+  methods: {
+  
+  }
+};
+</script>
+
+<style>
+#app {
+  padding-top: 40px;
+}
+</style>
